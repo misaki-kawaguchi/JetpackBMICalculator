4ecbfcc6c1802f941c6f8a542e1574839c38f375
misaki-kawaguchi <m.kawaguchi1122@gmail.com>
2023-09-09 22:07:21 +0900

    テキストフィールドのデータ管理

--- app/src/main/java/com/example/jetpackbmicalculator/MainViewModel.kt
+++ app/src/main/java/com/example/jetpackbmicalculator/MainViewModel.kt
@@ -1,7 +1,13 @@
 package com.example.jetpackbmicalculator
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 
 class MainViewModel : ViewModel() {
   val test = "test"
+
+  var height by mutableStateOf("")
+  var weight by mutableStateOf("")
 }
